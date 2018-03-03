36b859540653362828cec9918bdc0b3cb2901bb1
Kenneth Jones <kayceejjay@gmail.com>
2018-03-03 20:32:15 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end  
   
   def new
@@ -19,9 +20,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -47,14 +48,6 @@ class UsersController < ApplicationController
       redirect_to users_url
   end
 
-  private
-  
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-  public
-  
   # Before filters
 
     # Confirms a logged-in user.
@@ -76,5 +69,13 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
+
+ private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
+  
   
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -44,7 +44,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal assigns(:user).remember_token, cookies['remember_token'] # user.remember_token = cookies value 
+    assert_equal cookies['remember_token'], assigns(:user).remember_token # user.remember_token = cookies value 
     assert_not_empty cookies['remember_token']
   end
 
